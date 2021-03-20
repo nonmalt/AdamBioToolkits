db5258a65a935117cdf8c2c4da1c23327a95149f
Adam <mourns.dolen@gmail.com>
2021-03-20 23:39:35 +0800

    GutMicrobiomeHealthIndex Method Upgrade

--- evaluate/gmhi.py
+++ evaluate/gmhi.py
@@ -3,6 +3,20 @@ from joblib import Parallel, delayed
 from multiprocessing import cpu_count
 
 class GutMicrobiomeHealthIndex():
+    def __init__(self, ThetaF=0, ThetaD=0, n_jobs=1, low_abundance=1e-5, MaxF=2, MaxD=1, step=0.1):
+        self.ThetaF = ThetaF
+        self.ThetaD = ThetaD
+        self.low_abundance = low_abundance
+        self.MaxF = MaxF
+        self.MaxD = MaxD
+        self.step = step
+        
+        if n_jobs == -1:
+            self.n_jobs = cpu_count()
+        else:
+            self.n_jobs = n_jobs
+        
+        
     def __species_prevalence(self, sample, M):
         sample = sample.loc[:, M]
         return sample.sum() / sample.shape[0]
@@ -80,40 +94,44 @@ class GutMicrobiomeHealthIndex():
         xMM = self.__proportions_of_samples_classified(health, nonhealth, Mh, Mn)
         return xMM
         
-    def fit(self, health, nonhealth, ThetaF=0, ThetaD=0, n_jobs=1, low_abundance=1e-5, MaxF=2, MaxD=1, step=0.1):
+    def fit(self, health, nonhealth):
         # Filter
-        health[health < low_abundance] = 0
-        nonhealth[nonhealth < low_abundance] = 0
+        health[health < self.low_abundance] = 0
+        nonhealth[nonhealth < self.low_abundance] = 0
         
         # params search
-        if (ThetaF == 0) or (ThetaD == 0):
-            RangeThetaF = np.arange(1+step, MaxF, step)
-            RangeThetaD = np.arange(step, MaxD, step)
+        if (self.ThetaF == 0) or (self.ThetaD == 0):
+            RangeThetaF = np.arange(1+self.step, self.MaxF, self.step)
+            RangeThetaD = np.arange(self.step, self.MaxD, self.step)
             
             RangeThetaFD = []
             for F in RangeThetaF:
                 for D in RangeThetaD:
                     RangeThetaFD.append([F, D])
             
-            if n_jobs == -1:
-                n_jobs = cpu_count()
-            
-            ThetaSearch = Parallel(n_jobs=n_jobs)(delayed(self.xMM)(health, nonhealth, i[0], i[1]) for i in RangeThetaFD)
+            ThetaSearch = Parallel(n_jobs=self.n_jobs)(delayed(self.xMM)(health, nonhealth, i[0], i[1]) for i in RangeThetaFD)
             OptimTheta = RangeThetaFD[np.argmax(ThetaSearch)]
             self.ThetaF = OptimTheta[0]
             self.ThetaD = OptimTheta[1]
             self.accuracy = max(ThetaSearch)
-            
-        else:
-            self.ThetaF = ThetaF
-            self.ThetaD = ThetaD
-            self.accuracy = self.xMM(health, nonhealth, self.ThetaF, self.ThetaD)
 
         # get species list
-        self.Mh, self.Mn = self.__get_mlist(health, nonhealth, ThetaF, ThetaD)
+        self.Mh, self.Mn = self.__get_mlist(health, nonhealth, self.ThetaF, self.ThetaD)
 
     def transform(self, sample):
-        cabH = self.__collective_abundance(sample, self.Mh)
-        cabN = self.__collective_abundance(sample, self.Mn)
-        hiMM = self.__ratio_of_collective_abundance(cabH, cabN)
+        if type(sample) == pd.core.series.Series:
+            cabH = self.__collective_abundance(sample, self.Mh)
+            cabN = self.__collective_abundance(sample, self.Mn)
+            hiMM = self.__ratio_of_collective_abundance(cabH, cabN)
+        
+        elif type(sample) == pd.core.frame.DataFrame:
+            hiMM = []
+            for idx in sample.index:
+                samp = sample.loc[idx, :]
+                cabH = self.__collective_abundance(samp, self.Mh)
+                cabN = self.__collective_abundance(samp, self.Mn)
+                hiMMSingle = self.__ratio_of_collective_abundance(cabH, cabN)
+                hiMM.append(hiMMSingle)
+            
+            hiMM = pd.DataFrame(hiMM, index=sample.index, columns=['GMHI'])   
         return hiMM
